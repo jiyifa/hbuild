390d5a0e52286dc52520f6a03a53c4a42945a41a
1805j1 <1805j1@163.com>
2018-06-13 19:03:55 +0800

    框架的搭建

---
+++ src/main/java/com/example/entity/Name.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+
+public class Name {
+
+  private String name;
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
